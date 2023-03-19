f28a73b97bd526908b35fc4cc63650efa428b2b1
Rob <127467383+robertldev@users.noreply.github.com>
2023-03-19 15:01:24 +1100

    Completion of first 2 JS exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR'
+    } else {
+        let output = '';
+        for (; num > 0; num--) {
+            output += string;
+        }
+        return output
+    }
 };
 
 // Do not edit below this line
