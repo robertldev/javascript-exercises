ca4e89eb37b901b9235f5359b71ad155931f8c4f
Rob <127467383+robertldev@users.noreply.github.com>
2023-03-19 15:51:45 +1100

    Further JS exercises completed
    Exercises 1-4 now completed, 5-7 remaining.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let output = '';
+    for (let i = string.length - 1; i >= 0; i--) {
+        output+= string.charAt(i);
+    }
+    return output
 
+    /* This could also be done with string.split. . reverse and .join in one line */
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    for (let i= 1; i < args.length; i++) {
+        let index = args[0].indexOf(args[i]);
+        if (index > -1) {
+            args[0].splice(index, 1);
+        }
+    }
+    return args[0]
 };
 
 // Do not edit below this line
