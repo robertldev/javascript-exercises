c6bebd5032fdc2b6c51bb1329cdeef727e21645a
Rob <127467383+robertldev@users.noreply.github.com>
2023-03-29 22:19:19 +1100

    Completion of some further JS challenges for TOP.
    Nearly completed the calculator challenges.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,25 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce(function(acc, n) { return acc + n; }, 0); 
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce(function(acc, n) { return acc * n; }); 
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  
 };
 
 // Do not edit below this line
